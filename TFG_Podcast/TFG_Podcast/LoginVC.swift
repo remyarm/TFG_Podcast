911f8308c846be21007667b563e12558ec14eaeb
Remya R M <remya.murukesan@ing.com.au>
2020-06-20 11:56:51 +0530

    Initial Project setup

---
+++ TFG_Podcast/TFG_Podcast/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TFG_Podcast
+//
+//  Created by Murukesan, R. (Remya) on 20/6/20.
+//  Copyright © 2020 Murukesan, R. (Remya). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
