50a6ffa8cd684e0f050b325dd76cca68cd9f7ad9
Remya <retnammalmurukesanr@hcl.com>
2020-06-20 12:55:48 +0530

    Added URL

--- TFG_Podcast/TFG_Podcast/LoginVC.swift
+++ TFG_Podcast/TFG_Podcast/LoginVC.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, UIWebViewDelegate {
     @IBOutlet var loginWebView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "https://www.hackingwithswift.com")!
+        let url = URL(string: "https://hclo365.sharepoint.com/sites/KXTechOffice/TFG/_layouts/15/listfeed.aspx?List=%7bC6B5AE9B-02F2-49F8-A979-BE766A08893D%7d&Source=https%3a%2f%2fhclo365.sharepoint.com%2fsites%2fKXTechOffice%2fTFG%2fLists%2fTFG%2520TechTalks%2520Podcast%2520List%2fAllItems.aspx&CT=1592637866462&OR=OWA-NT&CID=7b866810-363e-3426-b19a-733db38b5757#1")!
         loginWebView.load(URLRequest(url: url))
         loginWebView.allowsBackForwardNavigationGestures = true
     }
