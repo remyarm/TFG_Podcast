8c27a91d54a7d0af0874200a9e4de9f481a27847
Remya <retnammalmurukesanr@hcl.com>
2020-07-14 10:34:18 +0530

    trying to extract RSS feed

--- TFG_Podcast/TFG_Podcast/LoginVC.swift
+++ TFG_Podcast/TFG_Podcast/LoginVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, UIWebViewDelegate {
+class ViewController: UIViewController, WKNavigationDelegate {
 
     @IBOutlet var loginWebView: WKWebView!
     override func viewDidLoad() {
@@ -17,8 +17,15 @@ class ViewController: UIViewController, UIWebViewDelegate {
         let url = URL(string: "https://hclo365.sharepoint.com/sites/KXTechOffice/TFG/_layouts/15/listfeed.aspx?List=%7bC6B5AE9B-02F2-49F8-A979-BE766A08893D%7d&Source=https%3a%2f%2fhclo365.sharepoint.com%2fsites%2fKXTechOffice%2fTFG%2fLists%2fTFG%2520TechTalks%2520Podcast%2520List%2fAllItems.aspx&CT=1592637866462&OR=OWA-NT&CID=7b866810-363e-3426-b19a-733db38b5757#1")!
         loginWebView.load(URLRequest(url: url))
         loginWebView.allowsBackForwardNavigationGestures = true
+        loginWebView.navigationDelegate = self
     }
-
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+//        print(webView.data)
+        webView.evaluateJavaScript("document.documentElement.outerHTML.toString()",
+                                   completionHandler: { (html: Any?, error: Error?) in
+                                    print(html ?? "")
+        })
+    }
 
 }
 
